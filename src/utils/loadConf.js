0aeba83a6a66779234ebc24da72a110101c4707c
jkzing <jingkai.zhao@foxmail.com>
2017-06-02 00:07:53 +0800

    remove useless dependencies

--- src/utils/loadConf.js
+++ src/utils/loadConf.js
@@ -1,14 +1,7 @@
-'use strict';
-
 const _ = require('lodash');
-const nconf = require('nconf');
-
-const clearUndefined = require('./index').clearUndefined;
 
 const defaults = require.resolve('../config/defaults.json');
 const config = require.resolve('../config/config.json');
-
-
 
 module.exports = function loadConf(args={}) {
     return _.merge(require(defaults), require(config), args);

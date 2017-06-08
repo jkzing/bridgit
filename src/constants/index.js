34a5fc3ad80ab6af5c150317ba954bbd60682c9d
jkzing <jingkai.zhao@foxmail.com>
2017-06-08 22:57:07 +0800

    change configuration file logic

---
+++ src/constants/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    configFileName: '.bridgit.json'
+}
--- src/utils/mergeConfiguration.js
+++ src/utils/mergeConfiguration.js
@@ -1,7 +1,9 @@
 const _ = require('lodash');
+const path = require('path');
+const os = require('os');
+const configFileName = require('../constants').configFileName;
 
-const defaults = require.resolve('../config/defaults.json');
-const config = require.resolve('../config/config.json');
+const defaultsPath = path.resolve('../config/defaults.json');
 
 // function validateArgs(args) {
 //     let len = args.length;
@@ -12,5 +14,13 @@ const config = require.resolve('../config/config.json');
  * no need for deep merge for now
  */
 module.exports = function merge(...args) {
-    return _.merge(require(defaults), require(config), ...args);
+    let configPath = path.join(os.homedir(), configFileName);
+    let storedConf, defaultsConf;
+    try {
+        storedConf = require(configPath);
+        defaultsConf = require(defaultsPath);
+    } catch (e) {}
+    storedConf = storedConf || {};
+    defaultsConf = defaultsConf || {};
+    return _.merge(defaultsConf, storedConf, ...args);
 }
